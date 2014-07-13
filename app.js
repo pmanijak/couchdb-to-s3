7aa99975e28cb3dcd59497851a2daa1b9d23f3e7
Phil Manijak <github@exclsr.com>
2014-07-12 21:24:23 -0700

    Use 'knox' for S3. Load config from file.
     * Use 'knox' for its 'putFile' method, for easy
       streaming of files to S3. 
     * Load credentials from a standard AWS 
       credentials file.
     * Specify other settings in config.json

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
 var knox  = require('knox');
-var couch  = require('./lib/couch.js');
 var nconf = require('nconf');
+var path  = require('path');
+var cred  = require('./lib/cred.js');
+var couch = require('./lib/couch.js');
 
-couch = couch('http://localhost:5984');
+nconf.argv().env().file('config.json');
 
-couch.getDatabaseDir(function (err, dir) {
+var dbSettings   = couch(nconf.get('dbUrl'));
+var dbName       = nconf.get('dbName');
+
+var awsCred      = cred(nconf.get('awsCredentialsPath'));
+var s3BucketName = nconf.get('s3BucketName');
+
+dbSettings.getDatabaseDir(function (err, dbDir) {
 	if (err) {
+		console.log("Could not connect to CouchDB.");
 		console.log(err);
 		return;
 	}
 
-	console.log(dir);
+	var client = knox.createClient({
+		key: awsCred.aws_access_key_id,
+		secret: awsCred.aws_secret_access_key,
+		bucket: s3BucketName
+	});
+
+	var dbFileExt = ".couch";
+	var dbFilePath = path.join(dbDir, dbName + dbFileExt);
+
+	console.log("Backing up database ");
+	console.log("    at " + dbFilePath + "...");
+
+	var s3FilePath = "/" + dbName + "-backup" + dbFileExt;
+
+	client.putFile(dbFilePath, s3FilePath, function (err, res) {
+		if (err) {
+			console.log("KNOX ERROR");
+			console.log(err);
+			return; 
+		}
+
+		if (res.statusCode === 200) {
+			console.log("SUCCESS");
+			console.log(res.statusCode);
+			console.log(res.headers);			
+		}
+		else {
+			console.log("FAILURE");
+			console.log(res.statusCode);
+			console.log(res.headers);
+		}
+	});
 });
